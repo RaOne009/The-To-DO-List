aafd23d4980a68fc9e0c89abeb0daf898e59aa64
RaOne009 <rs1257933@gmail.com>
2019-10-08 16:10:36 +0530

    .....

--- list.js
+++ list.js
@@ -1,1 +1,12 @@
-console.log("Tony")
+var todo = [];
+var input =  prompt("What would you like to do?");
+while (input !== "Q") {
+    if (input === "L") {
+        console.log(todo);
+    } else if (input === "N") {
+        var newtodo = prompt("Enter new todo");
+        todo.push(newtodo);
+    }
+    input = prompt("What would you like to do?");
+}
+console.log("Ok, You Quit the App");
